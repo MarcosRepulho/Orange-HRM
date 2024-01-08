503d2663da613386a060a5fec2a8879f1ea5ff4b
Marcos Repulho <marcos.repulho20@gmail.com>
2024-01-08 10:10:13 -0300

    First commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,20 @@
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get( "[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal','/web/index.php/dashboard/index')
+    cy.get(".oxd-topbar-header-breadcrumb-module").contains('Dashboard')
+  })
+  it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Test')
+    cy.get("[name='password']").type('teste123')
+    cy.get("[type='submit']").click()
+    cy.get("[role='alert']")
+  })
+
+
+
+})
